bae4c8ba7b7729c4e3f12b95981debd31793eb99
Maxime VÉRY <maximevery@ryvexam.fr>
2026-01-26 22:19:49 +0100

    Update version 0.0.9: revert to 2D-only Safari Ball model, keeping updated inventory and economy logic.

--- src/main/java/com/safari/client/SafariClientMod.java
+++ src/main/java/com/safari/client/SafariClientMod.java
@@ -4,21 +4,17 @@ import com.safari.SafariMod;
 import com.safari.entity.SafariEntities;
 import com.safari.entity.SafariNpcEntity;
 import com.safari.entity.SafariPortalNpcEntity;
-import com.safari.item.ModItems;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLayers;
-import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.util.Identifier;
 
 public class SafariClientMod implements ClientModInitializer {
     private static final Identifier NPC_TEXTURE = Identifier.of(SafariMod.MOD_ID, "textures/entity/safari_npc.png");
-    public static ModelTransformationMode currentMode = ModelTransformationMode.NONE;
 
     @Override
     public void onInitializeClient() {
@@ -38,13 +34,5 @@ public class SafariClientMod implements ClientModInitializer {
                     }
                 }
         );
-
-        // Register perspective predicate (0 = GUI/Other, 1 = Hand)
-        ModelPredicateProviderRegistry.register(ModItems.SAFARI_BALL, Identifier.of(SafariMod.MOD_ID, "perspective"), (stack, world, entity, seed) -> {
-            if (currentMode == ModelTransformationMode.GUI || currentMode == ModelTransformationMode.FIXED) {
-                return 0.0f;
-            }
-            return 1.0f;
-        });
     }
 }
